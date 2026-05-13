280ba88a1c1b40fb4db9cf9ba4fc1298aeb2655c
Matheus <matheusmartinscia@gmail.com>
2026-05-12 22:18:25 -0300

    Feat: Configurações iniciais

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if(DB::connection()->getPdo()) {
+        return 'Database connection is successful!';
+    } else {
+        return 'Database connection failed.';
+    }
 });
