f78565754d8432616df5df85a6a8b2927992a62e
Matheus <matheusmartinscia@gmail.com>
2026-05-14 20:18:53 -0300

    Feat: Cria modal Fintrack

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,40 @@
+import { TrendingUp } from 'lucide-react';
+import { Container } from '../../components/Container';
+import style from './style.module.css';
+import { useState } from 'react';
+
+export function Login() {
+
+  const [typeForm, setTypeForm] = useState<'Entrar' | 'Cadastrar'>('Entrar')
+
+  return (
+    <Container>
+      <div className={style.container}>
+
+        <div className={style.modal}>
+
+          <div className={style.header}>
+            <div className={style.containerIcon}>
+              <TrendingUp />
+            </div>
+
+            <h1 className={style.title}>Fin<span>track</span></h1>
+
+            <p className={style.description}>Controle financeiro pessoal</p>
+          </div>
+
+          <div className={style.selectForm}>
+            <div className={`${typeForm === 'Entrar' ? style.active : ''} ${style.selectTitleContainer}`} onClick={() => setTypeForm('Entrar')}>
+              <button type='button' className={`${style.selectTitle} ${typeForm === 'Entrar' ? style.textActive : ''}`} >Entrar</button>
+            </div>
+            
+            <div className={`${typeForm === 'Cadastrar' ? style.active : ''} ${style.selectTitleContainer}`} onClick={() => setTypeForm('Cadastrar')}>
+              <button type='button' className={`${style.selectTitle} ${typeForm === 'Cadastrar' ? style.textActive : ''}`} >Cadastrar</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </Container>
+  )
+}
