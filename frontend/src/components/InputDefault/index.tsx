793fe3abb9e2d80431cafac0d556ab86b70c54b3
Matheus <matheusmartinscia@gmail.com>
2026-05-14 22:15:00 -0300

    Feat: Melhora input pra senha

--- frontend/src/components/InputDefault/index.tsx
+++ frontend/src/components/InputDefault/index.tsx
@@ -1,18 +1,52 @@
-import type { ComponentProps } from "react"
+import { useState, type ComponentProps } from "react"
 import style from './style.module.css';
+import { EyeClosedIcon, EyeIcon } from "lucide-react";
 
 type InputDefaultPropos = {
-  label: string
+  label: string,
+  type: 'text' | 'number' | 'password' | 'email' | 'date'
 } & ComponentProps<'input'>
 
-export function InputDefault({ label, ...props }: InputDefaultPropos) {
+export function InputDefault({ label, type, ...props }: InputDefaultPropos) {
+
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === 'password';
+
 
   return (
     <>
-     <div className={style.inputContainer}>
-        <label htmlFor={label} className={style.label}>{label}</label>
-        <input id={label} className={style.input} {...props} />
-     </div>
+      <div className={style.inputContainer}>
+        <label htmlFor={label} className={style.label}>
+          {label}
+        </label>
+
+        <div className={style.inputContainerPassword}>
+          <input
+            id={label}
+            className={style.input}
+            type={
+              isPassword
+                ? (showPassword ? 'text' : 'password')
+                : type
+            }
+            {...props}
+          />
+
+          {isPassword && (
+            <button
+              type="button"
+              className={style.eyeButton}
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <EyeClosedIcon width={20} />
+              ) : (
+                <EyeIcon width={20} />
+              )}
+            </button>
+          )}
+        </div>
+      </div>
     </>
   );
 }
