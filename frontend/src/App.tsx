801575872a9b4f6282bfa63034de7c4a3088d69a
Matheus <matheusmartinscia@gmail.com>
2026-05-18 21:20:12 -0300

    Feat: Adiciona React Router Dom para navegação entre páginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Login } from "./pages/Login"
+import { Home } from "./pages/Home"
 
 function App() {
 
   return (
     <>
-     <Login />
+     <BrowserRouter>
+      <Routes>
+
+        <Route 
+          path="/"
+          element={<Login />}
+        />
+
+        <Route 
+          path="/home"
+          element={<Home />}
+        />
+
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
