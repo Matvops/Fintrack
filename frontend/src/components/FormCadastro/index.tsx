d5369248b93d0286cbd56210ec0f93c93f3f967e
Matheus <matheusmartinscia@gmail.com>
2026-05-18 21:06:35 -0300

    Feat: Cria componente de formulario de cadastro

---
+++ frontend/src/components/FormCadastro/index.tsx
@@ -0,0 +1,50 @@
+import { ButtonDefault } from '../ButtonDefault';
+import { InputDefault } from '../InputDefault';
+import style from './style.module.css';
+
+export function FormCadastro() {
+
+  return (
+    <form className={style.form}>
+
+      <div>
+        <InputDefault
+          label='NOME COMPLETO'
+          placeholder='José Da Silva'
+          type='text'
+        />
+      </div>
+
+      <div>
+        <InputDefault
+          label='E-MAIL'
+          placeholder='email@example.com'
+          type='email'
+        />
+      </div>
+
+      <div className={style.containerPassword}>
+        <InputDefault
+          label='Senha'
+          placeholder='admin123'
+          type='password'
+          autoComplete='off'
+        />
+
+         <InputDefault
+          label='Confirme a senha'
+          placeholder='admin123'
+          type='password'
+          autoComplete='off'
+        />
+      </div>
+
+      <div className={style.containerButton}>
+        <ButtonDefault
+          text='Cadastrar'
+        />
+      </div>
+
+    </form>
+  );
+}
