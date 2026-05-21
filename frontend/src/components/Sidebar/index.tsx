7d4d936e8dcd5d072f8fc9ed7ac411f3b6ef2aaf
Matheus <matheusmartinscia@gmail.com>
2026-05-20 22:20:42 -0300

    Feat: Adiciona navegação ao sair

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@ import { CreditCard, LayoutDashboard, LogOut, Settings, Target, Wallet } from "l
 import { Logo } from "../Logo";
 import style from './style.module.css';
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 type sections = 'Dashboard' | 'Transacoes' | 'Orcamento' | 'Metas' | 'Configuracoes';
 
@@ -12,46 +13,46 @@ export function Sidebar() {
 
   return (
     <div className={style.sidebar}>
-        <Logo size='medium' />
+      <Logo size='medium' />
 
-        <nav className={style.sections}>
-          <p 
-            className={`${style.section} ${activeSection === 'Dashboard' ? style.activeSection: ''}`} 
-            onClick={() => setActiveSection('Dashboard')}
-          >
-            <LayoutDashboard />Dashboard
-          </p>
-          <p 
-            className={`${style.section} ${activeSection === 'Transacoes' ? style.activeSection: ''}`} 
-            onClick={() => setActiveSection('Transacoes')}
-          >
-            <CreditCard />Transações
-          </p>
-          <p 
-            className={`${style.section} ${activeSection === 'Orcamento' ? style.activeSection: ''}`} 
-            onClick={() => setActiveSection('Orcamento')}
-          >
-            <Wallet />Orçamento
-          </p>
-          <p 
-            className={`${style.section} ${activeSection === 'Metas' ? style.activeSection: ''}`} 
-            onClick={() => setActiveSection('Metas')}
-          >
-            <Target />Metas
-          </p>
-          <p 
-            className={`${style.section} ${activeSection === 'Configuracoes' ? style.activeSection: ''}`} 
-            onClick={() => setActiveSection('Configuracoes')}
-          >
-            <Settings />Configurações
-          </p>
-        </nav>
+      <nav className={style.sections}>
+        <p
+          className={`${style.section} ${activeSection === 'Dashboard' ? style.activeSection : ''}`}
+          onClick={() => setActiveSection('Dashboard')}
+        >
+          <LayoutDashboard />Dashboard
+        </p>
+        <p
+          className={`${style.section} ${activeSection === 'Transacoes' ? style.activeSection : ''}`}
+          onClick={() => setActiveSection('Transacoes')}
+        >
+          <CreditCard />Transações
+        </p>
+        <p
+          className={`${style.section} ${activeSection === 'Orcamento' ? style.activeSection : ''}`}
+          onClick={() => setActiveSection('Orcamento')}
+        >
+          <Wallet />Orçamento
+        </p>
+        <p
+          className={`${style.section} ${activeSection === 'Metas' ? style.activeSection : ''}`}
+          onClick={() => setActiveSection('Metas')}
+        >
+          <Target />Metas
+        </p>
+        <p
+          className={`${style.section} ${activeSection === 'Configuracoes' ? style.activeSection : ''}`}
+          onClick={() => setActiveSection('Configuracoes')}
+        >
+          <Settings />Configurações
+        </p>
+      </nav>
 
-        <div className={style.logoutContainer}>
-          <p className={style.logout}>
-            <LogOut /> Sair
-          </p>
-        </div>
+      <div className={style.logoutContainer}>
+        <Link to={'/'} className={style.logout}>
+          <LogOut /> Sair
+        </Link>
       </div>
+    </div>
   );
 }
