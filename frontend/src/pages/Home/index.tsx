5a96cecae52f670de4849336fd377a7933ffed37
Matheus <matheusmartinscia@gmail.com>
2026-05-20 22:08:45 -0300

    Feat: Adiciona sections e sair

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,58 @@
+import { CreditCard, LayoutDashboard, LogOut, Settings, Target, Wallet } from 'lucide-react';
+import { Logo } from '../../components/Logo';
 import style from './style.module.css';
+import { useState } from 'react';
+
+type sections = 'Dashboard' | 'Transacoes' | 'Orcamento' | 'Metas' | 'Configuracoes';
 
 export function Home() {
 
+  const [activeSection, setActiveSection] = useState<sections>('Dashboard');
+
   return (
-    <>
-      <h1>EAI</h1>
-    </>
+    <div style={{ height: '100%', width: '100%' }}>
+      <div className={style.sidebar}>
+        <Logo size='medium' />
+
+        <nav className={style.sections}>
+          <p 
+            className={`${style.section} ${activeSection === 'Dashboard' ? style.activeSection: ''}`} 
+            onClick={() => setActiveSection('Dashboard')}
+          >
+            <LayoutDashboard />Dashboard
+          </p>
+          <p 
+            className={`${style.section} ${activeSection === 'Transacoes' ? style.activeSection: ''}`} 
+            onClick={() => setActiveSection('Transacoes')}
+          >
+            <CreditCard />Transações
+          </p>
+          <p 
+            className={`${style.section} ${activeSection === 'Orcamento' ? style.activeSection: ''}`} 
+            onClick={() => setActiveSection('Orcamento')}
+          >
+            <Wallet />Orçamento
+          </p>
+          <p 
+            className={`${style.section} ${activeSection === 'Metas' ? style.activeSection: ''}`} 
+            onClick={() => setActiveSection('Metas')}
+          >
+            <Target />Metas
+          </p>
+          <p 
+            className={`${style.section} ${activeSection === 'Configuracoes' ? style.activeSection: ''}`} 
+            onClick={() => setActiveSection('Configuracoes')}
+          >
+            <Settings />Configurações
+          </p>
+        </nav>
+
+        <div className={style.logoutContainer}>
+          <p className={style.logout}>
+            <LogOut /> Sair
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
