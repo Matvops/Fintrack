6c0564f92bcfcfeaf085fce22bcdd15b85d271f3
Matheus <matheusmartinscia@gmail.com>
2026-05-21 22:07:15 -0300

    Feat: Adiciona AuthService ao controller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller {
 
+    private AuthService $service;
+
+    function __construct(AuthService $service)
+    {
+        $this->service = $service;
+    }
 
     public function login(Request $request) {
 
