1afd76a7198b106680dafcba0a8c799d9fc070e0
Matheus <matheusmartinscia@gmail.com>
2026-05-21 21:30:37 -0300

    Feat: Cria migration para criar tabela de usuários

---
+++ backend/database/migrations/2026_05_21_204230_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('use_id')->autoIncrement();
+            $table->string('use_name', 50);
+            $table->string('use_email')->unique();
+            $table->string('use_verification_token')->unique();
+            $table->timestamp('use_email_verified_at')->nullable();
+            $table->text('use_password')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
