e59ce4a4c14d2fdc4569c59e8846931c2b677622
Matheus <matheusmartinscia@gmail.com>
2026-05-21 21:30:19 -0300

    Feat: Adiciona rota de registro

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,4 +9,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
