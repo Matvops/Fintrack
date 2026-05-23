46fe5f2cbe2c5822d0901adad44735d15a97987a
Matheus <matheusmartinscia@gmail.com>
2026-05-23 16:16:41 -0300

    Feat: Cria método para registrar usuário

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -2,20 +2,48 @@
 
 namespace App\Services;
 
+use App\Exceptions\ValidationException;
 use App\Models\User;
+use App\Utils\Response;
 
-class AuthService {
-
-
-    public function register(array $dados) {
-
-        $user = new User();
-        $user->use_name = $dados['name'];
-        $user->use_email = $dados['email'];
-        $user->use_password = bcrypt($dados['password']);
-        $user->save();
+class AuthService
+{
 
 
-        return 'Sucesso ao cadastrar';
+    public function register(array $dados): Response
+    {
+
+        try {
+
+            $this->validateEmail($dados['email']);
+
+            $user = new User();
+            $user->use_name = $dados['name'];
+            $user->use_email = $dados['email'];
+            $user->use_password = bcrypt($dados['password']);
+            $user->save();
+
+            return Response::getResponse(true, 'Usuário cadastrado com sucesso', code: 201);
+        } catch (ValidationException $e) {
+            return Response::getResponse(false, $e->getMessage(), code: 400);
+        }
+    }
+
+    private function validateEmail(string $email): void 
+    {
+        if(substr_count($email, '@') !== 1) throw new ValidationException('Email inválido');
+
+        $partsEmail = mb_split('@', $email);
+
+        $needles = ['&', '=', '\'', '&', '<', '>', ','];
+
+        foreach($needles as $needle) {
+            if(str_contains($email, $needle)) throw new ValidationException('Email inválido');
+        }
+
+        $domain = $partsEmail[1];
+
+        if(str_contains('..', $domain)) throw new ValidationException('Email inválido');
+ 
     }
 }
