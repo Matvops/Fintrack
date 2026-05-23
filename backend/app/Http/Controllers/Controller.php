8ea8093b8438cf4d25b62f10024267cd032a9de6
Matheus <matheusmartinscia@gmail.com>
2026-05-23 16:13:27 -0300

    Feat: Cria método para retornar valores da API

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Utils\Response;
+
 abstract class Controller
 {
-    //
+    protected function sendResponse(Response $response)
+    {
+        $content = [
+            'status' => $response->getStatus(),
+            'message' => $response->getMessage(),
+            'data' => $response->getData()
+        ];
+
+        $statusCode = (!is_null($response->getCode()) ?  $response->getCode() : $response->getStatus()) ? 200 : 500;  
+
+        return response($content, $statusCode);
+    }
 }
