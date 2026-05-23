e5a848183de2f6dd0425bf650048df0a0ba3cda2
Matheus <matheusmartinscia@gmail.com>
2026-05-23 16:14:19 -0300

    Feat: Remove prefixo '/api'

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
 
-
-Route::prefix('api')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-});
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
