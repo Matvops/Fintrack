41e621f201fe6ca21120fda29d1077cec8fc8752
Matheus <matheusmartinscia@gmail.com>
2026-05-24 17:04:28 -0300

    Feat: Cria estado inicial do User, no contexto

---
+++ frontend/src/contexts/initialState.ts
@@ -0,0 +1,10 @@
+import type { User } from "../types/User";
+
+export const initialState: User = {
+    id: null,
+    name: '',
+    email: '',
+    mainColor: 'ambar',
+    hiddenData: false,
+    plan: 'Free'
+}
