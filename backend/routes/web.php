e22364b75c32396349caf869976bad231125081d
Matheus <matheusmartinscia@gmail.com>
2026-05-24 23:46:20 -0300

    Feat: Adiciona rota para logout

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
