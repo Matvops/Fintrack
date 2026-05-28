2cea52edfd11e53b26f19f9972373181e792182e
Matheus <matheusmartinscia@gmail.com>
2026-05-28 20:28:43 -0300

    Feat: Cria rota para criar meta

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GoalsController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/goals/create', [GoalsController::class, 'create']);
