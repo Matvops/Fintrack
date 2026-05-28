dcb69806e7c344a5dde1460e84eb40f4d9c1900b
Matheus <matheusmartinscia@gmail.com>
2026-05-28 20:27:04 -0300

    Feat: Cria página de metas

---
+++ frontend/src/pages/Goals/index.tsx
@@ -0,0 +1,66 @@
+import { PlusIcon } from 'lucide-react';
+import { ButtonVisibleData } from '../../components/ButtonVisibleData';
+import { Heading } from '../../components/Heading';
+import { MainTemplate } from '../../templates/MainTemplate';
+import style from './style.module.css';
+import { ModalNewGoal } from './ModalNewGoal';
+import { useState } from 'react';
+import type { MainColor } from '../../types/MainColor';
+import { goals } from '../../services/goals';
+import { message } from '../../adapters/message';
+
+export type NewGoalData = {
+  id: number|null,
+  name: string,
+  balance: string,
+  balanceTarget: string,
+  color: MainColor
+};
+
+export function Goals() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  
+  async function cadastrar(data: NewGoalData, event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const response = await goals.create(data);
+    
+    if(response.status) {
+      message.success(response.message);
+      setModalVisible(false);
+      return;
+    }
+
+    message.error(response.message);
+  }
+
+  return (
+    <MainTemplate>
+
+      <div className={style.body}>
+        <header className={style.header}>
+          <Heading text='Metas' />
+
+          <ButtonVisibleData />
+        </header>
+
+        <section className={style.headerSection}>
+          <div className={style.textHeaderSection}>
+            <p>0 metas cadastradas</p>
+          </div>
+
+          <div onClick={() => setModalVisible(prevState => !prevState)}>
+            <button className={style.buttonHeaderSection}><PlusIcon /> Nova meta</button>
+          </div>
+        </section>
+
+        {modalVisible && (
+          <ModalNewGoal
+            setVisible={setModalVisible}
+            cadastrar={cadastrar}
+          />
+        )}
+      </div>
+    </MainTemplate>
+  );
+}
