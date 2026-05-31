04c95e7ae2633d1a4fb161c6dac4b0e6ca3f666f
Matheus <matheusmartinscia@gmail.com>
2026-05-31 16:46:55 -0300

    Feat: Cria modal para cadastrar novo orçamento

---
+++ frontend/src/pages/Budgets/ModalNewBudget/index.tsx
@@ -0,0 +1,73 @@
+
+import { Check, X } from 'lucide-react';
+import { Heading } from '../../../components/Heading';
+import style from './style.module.css';
+import { InputDefault } from '../../../components/InputDefault';
+import { useContext, useState } from 'react';
+import { Colors } from '../../../components/Colors';
+import type { MainColor } from '../../../types/MainColor';
+import { UserContext } from '../../../contexts/UserContext';
+import { formatToReal } from '../../../utils/formatToReal';
+import type { NewBudgetData } from '..';
+
+type ModalNewBudgetProps = {
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>,
+  create: (data: NewBudgetData, event: React.SubmitEvent<HTMLFormElement>) => void
+}
+
+export function ModalNewBudget({ setVisible, create }: ModalNewBudgetProps) {
+
+  const [name, setName] = useState('');
+  const [limit, setLimit] = useState('');
+  const [color, setColor] = useState<MainColor>('ambar');
+  const { user } = useContext(UserContext);
+  const [id] = useState(user.id);
+
+
+  return (
+    <div className={style.modalBackground}>
+      <div className={style.modal}>
+        <div className={style.heading}>
+          <Heading text='Novo orçamento' />
+
+          <div className={style.closeIcon} onClick={() => setVisible(prevState => !prevState)}>
+            <X />
+          </div>
+        </div>
+
+        <form className={style.form} onSubmit={(e) => create({id: id, name: name, limit: limit, color: color}, e)}>
+
+          <InputDefault
+            label='CATEGORIA'
+            placeholder='Ex: Alimentação'
+            type='text'
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+
+          <InputDefault
+            label='LIMITE MENSAL (R$)'
+            placeholder='0'
+            type='text'
+            onChange={e => setLimit(formatToReal(e.target.value))}
+            value={limit}
+          />
+
+          <div>
+            <h2 className={style.headerColor}>COR</h2>
+            <Colors
+              color={color}
+              setColor={setColor}
+            />
+          </div>
+
+          <div>
+            <button className={style.buttonHeaderSection}><Check /> Salvar</button>
+          </div>
+        </form>
+
+
+      </div>
+    </div>
+  );
+}
