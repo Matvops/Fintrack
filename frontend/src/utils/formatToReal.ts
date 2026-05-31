ce2fcd2f80c079d2f2c7ec04ca443927d3f34823
Matheus <matheusmartinscia@gmail.com>
2026-05-30 21:27:00 -0300

    Feat: Cria método para formatar string para real

---
+++ frontend/src/utils/formatToReal.ts
@@ -0,0 +1,14 @@
+export const formatToReal = (valor: string): string => {
+
+    const apenasNumeros = valor.replace(/\D/g, '');
+
+    let valorNumerico = Number(apenasNumeros);
+    if(valor.includes('.')) {
+        valorNumerico /=  100;
+    }
+
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(valorNumerico);
+};
