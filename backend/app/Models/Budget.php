c789c74e3df342ef9c560bcda6ced2280cc2e329
Matheus <matheusmartinscia@gmail.com>
2026-05-31 16:43:33 -0300

    Feat: Cria Model Budget

---
+++ backend/app/Models/Budget.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Budget extends Model
+{
+    public $table = 'budgets';
+    public $primaryKey = 'bdt_id';
+    public $timestamps = true;
+
+    public $fillable = [
+        'bdt_id',
+        'bdt_use_id',
+        'bdt_name',
+        'bdt_limit',
+        'bdt_current_expense',
+        'bdt_color',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'bdt_use_id');
+    }
+}
