7aa7d62c1884ccdd7b1814692e8c8943554daabe
Matheus <matheusmartinscia@gmail.com>
2026-05-30 21:22:39 -0300

    Feat: Adiciona rota para consultar metas

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,3 +10,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('/goals/create', [GoalsController::class, 'create']);
+Route::post('/goals/get', [GoalsController::class, 'getGoals']);
