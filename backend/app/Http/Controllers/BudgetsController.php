eb08c97920d4ab2dafe30dedc0fd23abe1dedf83
Matheus <matheusmartinscia@gmail.com>
2026-05-31 16:45:00 -0300

    Feat: Cria controller para cadastrar Budget

---
+++ backend/app/Http/Controllers/BudgetsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Budgets\CreateRequest;
+use App\Services\BudgetsService;
+
+class BudgetsController extends Controller {
+
+    private BudgetsService $service;
+
+    public function __construct(BudgetsService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function create(CreateRequest $request) {
+        
+        $dados = [
+            'id' => $request->input('id'),
+            'name' => $request->input('name'),
+            'limit' => $request->input('limit'),
+            'color' => $request->input('color'),
+        ];
+
+        $response = $this->service->create($dados);
+
+        return $this->sendResponse($response);
+    }
+}
