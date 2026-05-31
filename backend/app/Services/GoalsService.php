dc28225d8d7df64f14506d8d4c4f3f8de7209ef6
Matheus <matheusmartinscia@gmail.com>
2026-05-31 00:51:59 -0300

    Feat: Cria service para deletar meta

--- backend/app/Services/GoalsService.php
+++ backend/app/Services/GoalsService.php
@@ -10,7 +10,8 @@ use App\Repositories\UserRepository;
 use App\Utils\Response;
 use Exception;
 
-class GoalsService {
+class GoalsService
+{
 
     private UserRepository $userRepository;
     private GoalRepository $goalRepository;
@@ -25,10 +26,10 @@ class GoalsService {
     public function create(array $data): Response
     {
         try {
-            
+
             $user = $this->userRepository->getUserById($data['id']);
 
-            if(!isset($user)) throw new ValidationException('Erro ao criar meta', 404);
+            if (!isset($user)) throw new ValidationException('Erro ao criar meta', 404);
 
             $balance = $this->formatValue($data['balance']);
             $balanceTarget = $this->formatValue($data['balanceTarget']);
@@ -40,11 +41,11 @@ class GoalsService {
             $goal->gls_balance_target = str_replace(',', '.', $balanceTarget);
             $goal->gls_color = strtoupper($data['color']);
             $goal->save();
-            
+
             return Response::getResponse(true, 'Meta criada com sucesso');
-        } catch(ValidationException $e) {
+        } catch (ValidationException $e) {
             return Response::getResponse(false, $e->getMessage(), code: $e->getCode());
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             return Response::getResponse(false, 'Erro ao criar meta', code: 500);
         }
     }
@@ -53,32 +54,33 @@ class GoalsService {
     public function getGoals(int $id): Response
     {
         try {
-            
+
             $goals = $this->goalRepository->getGoalsByUseId($id);
 
-            if(count($goals) < 1) throw new NotFoundException("Sem metas");
-            
-            foreach($goals as $goal) {
+            if (count($goals) < 1) throw new NotFoundException("Sem metas");
+
+            foreach ($goals as $goal) {
                 $goal->percentage = $this->getPercentage($goal);
                 $goal->missing =  $goal->gls_balance_target - $goal->gls_balance;
             }
 
-            return Response::getResponse(true, 'Meta criada com sucesso', $goals);
-        } catch(NotFoundException $e) {
+            return Response::getResponse(true, 'Metas encontradas', $goals);
+        } catch (NotFoundException $e) {
             return Response::getResponse(false, $e->getMessage(), code: $e->getCode());
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             return Response::getResponse(false, 'Metas não localizadas', code: 500);
         }
     }
 
-    private function getPercentage($goal) {
+    private function getPercentage($goal)
+    {
         return floor($goal->gls_balance / $goal->gls_balance_target * 100);
-    } 
+    }
 
-     public function edit(array $request): Response
+    public function edit(array $request): Response
     {
         try {
-            
+
             $goal = $this->goalRepository->getGoalById($request['gls_id']);
 
             $goal->gls_name = $request['gls_name'];
@@ -88,14 +90,30 @@ class GoalsService {
             $goal->save();
 
             return Response::getResponse(true, 'Meta editada com sucesso');
-        } catch(NotFoundException $e) {
+        } catch (NotFoundException $e) {
             return Response::getResponse(false, $e->getMessage(), code: $e->getCode());
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             return Response::getResponse(false, 'Metas não localizadas', code: 500);
         }
     }
 
-    private function formatValue(string $value) {
+    private function formatValue(string $value)
+    {
         return preg_replace('/[^0-9.]/', '', str_replace(',', '.', str_replace('.', '', $value)));
+    }
+
+    public function delete(int $id): Response
+    {
+        try {
+
+            $goal = $this->goalRepository->getGoalById($id);
+            $goal->delete();
+
+            return Response::getResponse(true, 'Meta excluída com sucesso');
+        } catch (NotFoundException $e) {
+            return Response::getResponse(false, $e->getMessage(), code: $e->getCode());
+        } catch (Exception $e) {
+            return Response::getResponse(false, 'Metas não localizadas', code: 500);
+        }
     }
 }
