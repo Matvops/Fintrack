4d6cc97ab1c5e1b198f427cbafeb2a7eced9caa4
Matheus <matheusmartinscia@gmail.com>
2026-05-31 16:44:06 -0300

    Feat: Cria service para Budgets

---
+++ backend/app/Services/BudgetsService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Budget;
+use App\Utils\Functions;
+use App\Utils\Response;
+use Exception;
+
+class BudgetsService {
+
+
+    public function create(array $request): Response
+    {
+        try {
+            
+            $budget = new Budget();
+            $budget->bdt_use_id = $request['id'];
+            $budget->bdt_name = $request['name'];
+            $budget->bdt_limit = Functions::formatValue($request['limit']);
+            $budget->bdt_color = strtoupper($request['color']);
+            $budget->bdt_current_expense = 0;
+            $budget->save();
+
+            return Response::getResponse(true, 'Orçamento criado com sucesso', code: 201);
+        } catch(Exception $e) {
+            return Response::getResponse(false, 'Erro ao criar novo orçamento', code: $e->getCode());
+        }
+    }
+}
