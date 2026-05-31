dea6db546d749dcddabd62f84b0687b23d0f9de4
Matheus <matheusmartinscia@gmail.com>
2026-05-30 21:30:42 -0300

    Fix: Corrige formatação para real

--- frontend/src/utils/formatToReal.ts
+++ frontend/src/utils/formatToReal.ts
@@ -3,7 +3,7 @@ export const formatToReal = (valor: string): string => {
     const apenasNumeros = valor.replace(/\D/g, '');
 
     let valorNumerico = Number(apenasNumeros);
-    if(valor.includes('.')) {
+    if(valor.includes('.') || valor.includes(',')) {
         valorNumerico /=  100;
     }
 
