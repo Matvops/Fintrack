456c7eb1c53cafb84fd42bd79a748a85f26b97a0
Matheus <matheusmartinscia@gmail.com>
2026-05-31 00:17:15 -0300

    Feat: Cria método para consultar usuário por id

--- backend/app/Repositories/GoalRepository.php
+++ backend/app/Repositories/GoalRepository.php
@@ -6,7 +6,13 @@ use App\Models\Goal;
 
 class GoalRepository {
 
-    public function getGoalRepository(int $id) {
+    public function getGoalsByUseId(int $id) {
         return Goal::where('gls_use_id', $id)->orderBy('created_at', 'DESC')->get();
+    }
+
+
+    public function getGoalById(int $id): ?Goal
+    {
+        return Goal::where('gls_id', $id)->first();
     }
 }
