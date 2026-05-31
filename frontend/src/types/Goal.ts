0c008fb3777cabad9412e1673a34e19c3f4ed7bc
Matheus <matheusmartinscia@gmail.com>
2026-05-31 00:18:54 -0300

    Feat: Altera types dos atributos da interface Goal

--- frontend/src/types/Goal.ts
+++ frontend/src/types/Goal.ts
@@ -2,14 +2,14 @@ import type { MainColor } from "./MainColor";
 
 
 export interface Goal {
-    created_at: string|number,
-    updated_at: string|number,
     gls_balance: string,
     gls_balance_target: string,
     gls_color: MainColor,
     gls_id: number,
     gls_name: string,
-    gls_use_id: number,
+    gls_use_id?: number,
     missing?: string,
-    percentage?: number
+    percentage?: number,
+    created_at?: string|number,
+    updated_at?: string|number,
 }
