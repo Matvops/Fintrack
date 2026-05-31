3402f65912ae4c6a3cf85823fe4c44aa0ae289f5
Matheus <matheusmartinscia@gmail.com>
2026-05-31 16:45:40 -0300

    Feat: Cria service para os budgets

---
+++ frontend/src/services/budget.ts
@@ -0,0 +1,85 @@
+import type { ResponseApi } from "../interfaces/ResponseApi";
+import type { NewBudgetData } from "../pages/Budgets";
+import type { Goal } from "../types/Goal";
+import { getMessageError } from "../utils/getMessageError";
+import api from "./api";
+
+
+export const budget = {
+
+    async create(data: NewBudgetData): Promise<ResponseApi>  
+    {
+
+        try {
+            const response = await api.post("/budgets/create", data);
+
+            return response.data;
+        } catch (error: unknown) {
+
+            const message = getMessageError(error);
+
+            return {
+                status: false,
+                message: message,
+                data: {}
+            };
+        }
+    },
+
+    async edit(data: Goal): Promise<ResponseApi>  
+    {
+
+        try {
+            const response = await api.post("/budgets/edit", data);
+
+            return response.data;
+        } catch (error: unknown) {
+
+            const message = getMessageError(error);
+
+            return {
+                status: false,
+                message: message,
+                data: {}
+            };
+        }
+    },
+
+    async delete(id: number): Promise<ResponseApi>  
+    {
+
+        try {
+            const response = await api.post("/budgets/delete", {id});
+
+            return response.data;
+        } catch (error: unknown) {
+
+            const message = getMessageError(error);
+
+            return {
+                status: false,
+                message: message,
+                data: {}
+            };
+        }
+    },
+
+    async get(id: number|null): Promise<ResponseApi> 
+    {
+
+        try {
+            const response = await api.post("/budgets/get", {id});
+
+            return response.data;
+        } catch (error: unknown) {
+
+            const message = getMessageError(error);
+
+            return {
+                status: false,
+                message: message,
+                data: {}
+            };
+        }
+    }
+}
