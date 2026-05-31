6170508894741182e5f2cbe0798f9d95bcc107c7
Matheus <matheusmartinscia@gmail.com>
2026-05-30 21:24:11 -0300

    Feat: Cria service para consultar metas

--- backend/app/Services/GoalsService.php
+++ backend/app/Services/GoalsService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services;
 
+use App\Exceptions\NotFoundException;
 use App\Exceptions\ValidationException;
 use App\Models\Goal;
+use App\Repositories\GoalRepository;
 use App\Repositories\UserRepository;
 use App\Utils\Response;
 use Exception;
@@ -11,10 +13,12 @@ use Exception;
 class GoalsService {
 
     private UserRepository $userRepository;
+    private GoalRepository $goalRepository;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, GoalRepository $goalRepository)
     {
         $this->userRepository = $userRepository;
+        $this->goalRepository = $goalRepository;
     }
 
 
@@ -26,8 +30,8 @@ class GoalsService {
 
             if(!isset($user)) throw new ValidationException('Erro ao criar meta', 404);
 
-            $balance = preg_replace('/[^0-9.]/', '', str_replace(',', '.', $data['balance']));
-            $balanceTarget = preg_replace('/[^0-9.]/', '', str_replace(',', '.', $data['balanceTarget']));
+            $balance = preg_replace('/[^0-9.]/', '', str_replace('.', '', str_replace(',', '.', $data['balance'])));
+            $balanceTarget = preg_replace('/[^0-9.]/', '', str_replace('.', '', str_replace(',', '.', $data['balanceTarget'])));
 
             $goal = new Goal();
             $goal->gls_use_id = $user->use_id;
@@ -44,4 +48,30 @@ class GoalsService {
             return Response::getResponse(false, 'Erro ao criar meta', code: 500);
         }
     }
+
+
+    public function getGoals(int $id): Response
+    {
+        try {
+            
+            $goals = $this->goalRepository->getGoalRepository($id);
+
+            if(count($goals) < 1) throw new NotFoundException("Sem metas");
+            
+            foreach($goals as $goal) {
+                $goal->percentage = $this->getPercentage($goal);
+                $goal->missing =  $goal->gls_balance_target - $goal->gls_balance;
+            }
+
+            return Response::getResponse(true, 'Meta criada com sucesso', $goals);
+        } catch(NotFoundException $e) {
+            return Response::getResponse(false, $e->getMessage(), code: $e->getCode());
+        } catch(Exception $e) {
+            return Response::getResponse(false, 'Metas não localizadas', code: 500);
+        }
+    }
+
+    private function getPercentage($goal) {
+        return $goal->gls_balance / $goal->gls_balance_target * 100;
+    } 
 }
