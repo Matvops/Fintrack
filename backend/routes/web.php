b1248c0150bd1b34362d89341a29b3f20aaec380
Matheus <matheusmartinscia@gmail.com>
2026-05-31 00:14:19 -0300

    Feat: Cria método para editar Goal

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,4 +10,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('/goals/create', [GoalsController::class, 'create']);
+Route::post('/goals/edit', [GoalsController::class, 'edit']);
 Route::post('/goals/get', [GoalsController::class, 'getGoals']);
