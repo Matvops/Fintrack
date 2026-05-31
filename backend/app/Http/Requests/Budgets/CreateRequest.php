998185f3b03a0f76474f163f730788c25d01cbaf
Matheus <matheusmartinscia@gmail.com>
2026-05-31 16:45:15 -0300

    Feat: Cria validação de Request para validar criação de orçamento

---
+++ backend/app/Http/Requests/Budgets/CreateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Budgets;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|exists:users,use_id',
+            'name' => 'required',
+            'limit' => 'required',
+            'color' => 'required|in:ambar,violeta,esmeralda,rosa,azul',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'id.required' => 'O id é obrigatório',
+            'id.exists' => 'Erro ao criar orçamento',
+            'name.required' => 'O nome é obrigatório',
+            'limit.required' => 'O limite é obrigatório',
+            'color.required' => 'A cor é obrigatória',
+            'color.in' => 'A cor é inválida'
+        ];
+    }
+}
