034158328c1be023ba574ea476496d2f55bb2135
Matheus <matheusmartinscia@gmail.com>
2026-05-31 23:15:04 -0300

    Feat: Cria validação da request para consultar orçamentos

---
+++ backend/app/Http/Requests/Budgets/GetBudgetsRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Budgets;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetBudgetsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|exists:users,use_id',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'id.required' => 'O id é obrigatório',
+            'id.exists' => 'Erro ao listar orçamentos',
+        ];
+    }
+}
