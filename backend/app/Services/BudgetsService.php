c4a1ff7095bc80b1249fa76693ab988882857891
Matheus <matheusmartinscia@gmail.com>
2026-05-31 23:15:17 -0300

    Feat: Cria service para consultar budgets

--- backend/app/Services/BudgetsService.php
+++ backend/app/Services/BudgetsService.php
@@ -2,12 +2,21 @@
 
 namespace App\Services;
 
+use App\Exceptions\NotFoundException;
 use App\Models\Budget;
+use App\Repositories\BudgetRepository;
 use App\Utils\Functions;
 use App\Utils\Response;
 use Exception;
 
 class BudgetsService {
+
+    private BudgetRepository $budgetRepository;
+
+    public function __construct(BudgetRepository $budgetRepository)
+    {
+        $this->budgetRepository = $budgetRepository;
+    }
 
 
     public function create(array $request): Response
@@ -25,6 +34,20 @@ class BudgetsService {
             return Response::getResponse(true, 'Orçamento criado com sucesso', code: 201);
         } catch(Exception $e) {
             return Response::getResponse(false, 'Erro ao criar novo orçamento', code: $e->getCode());
+        }
+    }
+
+    public function get(int $id): Response
+    {
+        try {
+            
+            $budgets = $this->budgetRepository->getBudgetsByUseId($id);
+
+            if (count($budgets) < 1) throw new NotFoundException("Sem Orçamentos");
+
+            return Response::getResponse(true, 'Orçamentos encontrados', $budgets);
+        } catch(Exception $e) {
+            return Response::getResponse(false, 'Erro ao localizar orçamentos', code: $e->getCode());
         }
     }
 }
