e14e4e8c6fa7d6eff665f402cc5afe8fc6dd901d
Matheus <matheusmartinscia@gmail.com>
2026-05-31 23:11:59 -0300

    Feat: Cria componente BudgetList

---
+++ frontend/src/components/BudgetsList/index.tsx
@@ -0,0 +1,46 @@
+import { Pencil } from 'lucide-react';
+import style from './style.module.css';
+import type { Budget } from '../../types/Budget';
+import { formatToReal } from '../../utils/formatToReal';
+
+type BudgetsListProps = {
+  budgets: Budget[]
+}
+
+export function BudgetsList({ budgets }: BudgetsListProps) {
+
+  const get = () => {
+
+    return budgets.map((budget, index) => (
+      <div className={style.card} key={index}>
+        <div className={style.headerCard}>
+
+          <h2 className={style.headerTitle}>{budget.bdt_name}</h2>
+
+          <div className={style.cardValues}>
+            <div>
+              <span className={style.headerSubTitle}>R$ 515,00 / {formatToReal(budget.bdt_limit.toString())}</span>
+            </div>
+            <button className={style.buttonEdit} onClick={() => { }}>
+              <Pencil /> Editar
+            </button>
+          </div>
+        </div>
+
+        <progress className={`${style.progressBar} ${style['background-' + budget.bdt_color.toLowerCase()]}`} value={0.74} />
+
+        <div className={style.footerCard}>
+          <span className={style.headerSubTitle}>Restam R$ 185,00</span>
+          <span className={`${style.percentage} ${style[budget.bdt_color.toLowerCase()]}`}>74%</span>
+        </div>
+      </div>
+    ));
+
+  };
+
+  return (
+    <main className={style.budgetsList}>
+      {get()}
+    </main>
+  );
+}
