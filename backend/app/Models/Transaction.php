42721a42a62b8c2f0f6cbc6b9943147f1f7c5cd6
Matheus <matheusmartinscia@gmail.com>
2026-06-02 21:51:59 -0300

    Feat: Cria model transaction

---
+++ backend/app/Models/Transaction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    public $table = 'transactions';
+    public $primaryKey = 'tra_id';
+    public $timestamps = true;
+
+    public $fillable = [
+        'tra_id',
+        'tra_bdt_id',
+        'tra_descricao',
+        'tra_value',
+        'tra_date',
+        'tra_type',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function budget()
+    {
+        return $this->belongsTo(Budget::class, 'tra_bdt_id');
+    }
+}
