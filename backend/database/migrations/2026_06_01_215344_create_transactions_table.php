a6eb9b9ef9ddbfe21e0241fbc0137923518087ce
Matheus <matheusmartinscia@gmail.com>
2026-06-02 21:53:58 -0300

    Feat: Cria migration para criar tabela 'transactions'

---
+++ backend/database/migrations/2026_06_01_215344_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id('tra_id')->autoIncrement();
+            $table->string('tra_descricacao');
+            $table->foreignId('tra_bdt_id')
+                    ->nullable()
+                    ->references('bdt_id')
+                    ->on('budgets');
+            $table->decimal('tra_value', 10, 2);
+            $table->timestamp('tra_date');
+            $table->enum('tra_type', ['expense', 'income']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
