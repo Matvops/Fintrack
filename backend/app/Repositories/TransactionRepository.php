87991c0d32aefe5a541db00af3cd518f39897b1f
Matheus <matheusmartinscia@gmail.com>
2026-06-11 20:13:55 -0300

    Feat: Cria método para consultar transações vinculadas a um orçamento/categoria

--- backend/app/Repositories/TransactionRepository.php
+++ backend/app/Repositories/TransactionRepository.php
@@ -6,8 +6,12 @@ use App\Models\Transaction;
 
 class TransactionRepository {
 
-    public function getBudgetsByUseId(int $useId) 
+    public function getTransactionsByUseId(int $useId) 
     {
         return Transaction::with('budget')->where('tra_use_id', $useId)->orderBy('tra_date', 'DESC')->get();
     }
+
+    public function getTransactionsByBudgetId(int $budgetId) {
+        return Transaction::where('tra_bdt_id', $budgetId)->get();
+    } 
 }
