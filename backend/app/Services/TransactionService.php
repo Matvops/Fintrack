65b185a8ef47f2c659ad9b1964f7b10a98656f3b
Matheus <matheusmartinscia@gmail.com>
2026-06-10 23:12:15 -0300

    Feat: Cria service para transaction

--- backend/app/Services/TransactionService.php
+++ backend/app/Services/TransactionService.php
@@ -2,31 +2,56 @@
 
 namespace App\Services;
 
+use App\Exceptions\NotFoundException;
 use App\Models\Transaction;
+use App\Repositories\TransactionRepository;
 use App\Utils\Functions;
 use App\Utils\Response;
 use Carbon\Carbon;
 use Exception;
 
-class TransactionService {
+class TransactionService
+{
+
+    private TransactionRepository $transactionRepository;
+
+    public function __construct(TransactionRepository $transactionRepository)
+    {
+        $this->transactionRepository = $transactionRepository;
+    }
+
 
     public function create(array $data): Response
     {
         try {
-            
+
             $transaction = new Transaction();
             $transaction->tra_use_id = $data['id'];
             $transaction->tra_bdt_id = $data['category'];
             $transaction->tra_description = $data['description'];
             $transaction->tra_value = Functions::formatValue($data['value']);
-            $transaction->tra_date = Carbon::parse("{$data['date']} 00:00:00")->toDateTimeString(); 
-            $transaction->tra_type = strtoupper($data['type']); 
+            $transaction->tra_date = Carbon::parse("{$data['date']} 00:00:00")->toDateTimeString();
+            $transaction->tra_type = strtoupper($data['type']);
             $transaction->save();
 
             return Response::getResponse(true, 'Transação cadastrada com sucesso');
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             error_log($e->getMessage());
             return Response::getResponse(false, 'Erro ao cadastrar transação');
+        }
+    }
+
+    public function getByUseId(int $id): Response
+    {
+        try {
+
+            $transactions = $this->transactionRepository->getBudgetsByUseId($id);
+
+            if (count($transactions) < 1) throw new NotFoundException("Sem Transações");
+
+            return Response::getResponse(true, 'Transações encontradas', $transactions);
+        } catch (Exception $e) {
+            return Response::getResponse(false, 'Erro ao localizar transações', code: $e->getCode());
         }
     }
 }
