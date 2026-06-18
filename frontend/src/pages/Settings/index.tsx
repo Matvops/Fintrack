5c59316894b4c7f1b44b77210ca42d5b10846177
Matheus <matheusmartinscia@gmail.com>
2026-06-17 21:20:23 -0300

    Feat: Cria page de configurações

---
+++ frontend/src/pages/Settings/index.tsx
@@ -0,0 +1,76 @@
+import { useContext, useState } from "react";
+import { ButtonVisibleData } from "../../components/ButtonVisibleData";
+import { Heading } from "../../components/Heading";
+import { MainTemplate } from "../../templates/MainTemplate";
+import style from './style.module.css';
+import type { MainColor } from "../../types/MainColor";
+import { Colors } from "../../components/Colors";
+import { UserContext } from "../../contexts/UserContext";
+import { auth } from "../../services/auth";
+import { initialState } from "../../contexts/initialState";
+import { useNavigate } from "react-router-dom";
+
+export function Settings() {
+
+  const { user, setUser } = useContext(UserContext);
+  const [color, setColor] = useState<MainColor>('ambar');
+  const navigation = useNavigate();
+
+
+  async function logout() {
+    await auth.logOut({id: user.id});
+
+    setUser(initialState);
+    navigation('/');
+  }
+
+  return (
+    <MainTemplate>
+      <div className={style.body}>
+        <header className={style.header}>
+          <Heading text='Configurações' />
+          <ButtonVisibleData />
+        </header>
+
+        <main className={style.cards}>
+          <section className={style.card}>
+            <div className={style.colors}>
+              <h1>Cor de destaque</h1>
+              <Colors
+                color={color}
+                setColor={setColor}
+              />
+            </div>
+          </section>
+
+          <section className={style.card}>
+            <div className={style.account}>
+              <h1>Conta</h1>
+              <div className={style.accountInfo}>
+                <div className={style.info}>
+                  <span className={style.label}>Nome</span>
+                  <span className={style.value}>{user.name}</span>
+                </div>
+                <div className={style.info}>
+                  <span className={style.label}>E-mail</span>
+                  <span className={style.value}>{user.email}</span>
+                </div>
+                <div className={style.info}>
+                  <span className={style.label}>Plano</span>
+                  <span className={style.value}>{user.plan}</span>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className={style.card}>
+            <div className={style.session}>
+              <h1>Sessão</h1>
+              <button onClick={() => logout()} type="button" className={style.button}>Sair da conta</button>
+            </div>
+          </section>
+        </main>
+      </div>
+    </MainTemplate>
+  );
+}
