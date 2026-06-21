7d5ea163901a110a3f0779f85511357495522787
Matheus <matheusmartinscia@gmail.com>
2026-06-21 17:25:11 -0300

    Feat: Adiciona condicional para consultar apenas os orçamentos do mês selecionado

--- backend/app/Repositories/BudgetRepository.php
+++ backend/app/Repositories/BudgetRepository.php
@@ -6,8 +6,11 @@ use App\Models\Budget;
 
 class BudgetRepository {
 
-    public function getBudgetsByUseId(int $id) {
-        return Budget::where('bdt_use_id', $id)->orderBy('created_at', 'DESC')->get();
+    public function getBudgetsByUseId(int $id, string $initialDate, string $finishDate) {
+        return Budget::where('bdt_use_id', $id)
+                        ->whereBetween('created_at', [$initialDate, $finishDate])
+                        ->orderBy('created_at', 'DESC')
+                        ->get();
     }
 
     public function getBudgetById(int $id): ?Budget
