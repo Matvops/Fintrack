eab74316a5ec3a610aec0fae4b49f80d54dafd0a
Matheus <matheusmartinscia@gmail.com>
2026-06-21 16:57:57 -0300

    Feat: Adiciona condicional para consultar transações do mês selecionado

--- backend/app/Repositories/TransactionRepository.php
+++ backend/app/Repositories/TransactionRepository.php
@@ -6,9 +6,13 @@ use App\Models\Transaction;
 
 class TransactionRepository {
 
-    public function getTransactionsByUseId(int $useId) 
+    public function getTransactionsByUseId(int $useId, string $initialDate, string $finishDate) 
     {
-        return Transaction::with('budget')->where('tra_use_id', $useId)->orderBy('tra_date', 'DESC')->get();
+        return Transaction::with('budget')
+                            ->where('tra_use_id', $useId)
+                            ->whereBetween('tra_date', [$initialDate, $finishDate])
+                            ->orderBy('tra_date', 'DESC')
+                            ->get();
     }
 
     public function getTransactionsByBudgetId(int $budgetId) {
