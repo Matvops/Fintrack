1f1e64b646c8bd266026871f0f31ff2228203408
Matheus <matheusmartinscia@gmail.com>
2026-07-16 20:25:09 -0300

    Feat: Cria métodos para teste do login

--- backend/tests/Unit/AuthServiceTest.php
+++ backend/tests/Unit/AuthServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Dto\Auth\LoginDTO;
+use App\Dto\User\UserDTO;
 use App\Models\User;
 use App\Repositories\UserRepository;
 use App\Services\AuthService;
@@ -78,5 +80,102 @@ class AuthServiceTest extends TestCase {
 
         $this->assertFalse($response->getStatus());
         $this->assertSame('Erro ao realizar Logout', $response->getMessage());
+    }
+
+    public function test_login_successfully(): void 
+    {
+
+        $logInvoker = Mockery::mock('alias:Illuminate\Support\Facades\Auth');
+        $logInvoker->shouldReceive('login')->andReturnNull();
+
+
+        $logInvoker = Mockery::mock('alias:App\Logging\LogInvoker');
+        $logInvoker->shouldReceive('login->withPayload->withResponse->save')->andReturnNull();
+
+        $email = 'example@gmail.com';
+
+        $user = new User();
+        $user->use_name = 'Example';
+        $user->use_email = $email;
+        $user->use_id = 1;
+        $user->use_password = password_hash('password', PASSWORD_BCRYPT);
+
+        $this->userRepository->shouldReceive('getUserByEmail')
+                            ->once()
+                            ->with($email)
+                            ->andReturn($user);
+
+        $loginDTO = LoginDTO::fromArray([
+            'email' => $email,
+            'password' => 'password'
+        ]);
+
+        $response = $this->authService->login($loginDTO);
+
+        $this->assertTrue($response->getStatus());
+        $this->assertSame('Login realizado com Sucesso!', $response->getMessage());
+        $this->assertNotNull($response->getData());
+
+        $userDTO = UserDTO::fromModel($user);
+        $this->assertSame($userDTO->toArray(), $response->getData());
+
+    }
+
+    public function test_login_with_invalid_email(): void 
+    {
+
+
+        $logInvoker = Mockery::mock('alias:App\Logging\LogInvoker');
+        $logInvoker->shouldReceive('login->withPayload->save')->andReturnNull();
+
+        $email = 'example@gmail.com';
+
+        $this->userRepository->shouldReceive('getUserByEmail')
+                            ->once()
+                            ->with($email)
+                            ->andReturnNull();
+
+        $loginDTO = LoginDTO::fromArray([
+            'email' => $email,
+            'password' => 'password'
+        ]);
+
+        $response = $this->authService->login($loginDTO);
+
+        $this->assertFalse($response->getStatus());
+        $this->assertSame('E-mail ou senha inválidos', $response->getMessage());
+        $this->assertSame(400, $response->getCode());
+    }
+
+    public function test_login_with_incorrect_password(): void 
+    {
+
+
+        $logInvoker = Mockery::mock('alias:App\Logging\LogInvoker');
+        $logInvoker->shouldReceive('login->withPayload->save')->andReturnNull();
+
+        $email = 'example@gmail.com';
+
+        $user = new User();
+        $user->use_name = 'Example';
+        $user->use_email = $email;
+        $user->use_id = 1;
+        $user->use_password = password_hash('password2', PASSWORD_BCRYPT);
+
+        $this->userRepository->shouldReceive('getUserByEmail')
+                            ->once()
+                            ->with($email)
+                            ->andReturn($user);
+
+        $loginDTO = LoginDTO::fromArray([
+            'email' => $email,
+            'password' => 'password'
+        ]);
+
+        $response = $this->authService->login($loginDTO);
+
+        $this->assertFalse($response->getStatus());
+        $this->assertSame('E-mail ou senha inválidos', $response->getMessage());
+        $this->assertSame(400, $response->getCode());
     }
 }
