c3afac9808513ea34ba0aa0e15050541658c72ef
Matheus <matheusmartinscia@gmail.com>
2026-07-26 01:23:01 -0300

    Feat: Cria método register no repository

--- backend/app/Repositories/BudgetRepository.php
+++ backend/app/Repositories/BudgetRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Dto\Budget\CreateBudgetDTO;
 use App\Models\Budget;
 
 class BudgetRepository {
@@ -16,5 +17,18 @@ class BudgetRepository {
     public function getBudgetById(int $id): ?Budget
     {
         return Budget::where('bdt_id', $id)->first();
+    }
+
+    public function register(CreateBudgetDTO $dto): Budget
+    {
+        $budget = new Budget();
+        $budget->bdt_use_id = $dto->id;
+        $budget->bdt_name = $dto->name;
+        $budget->bdt_limit = $dto->limit;
+        $budget->bdt_color = $dto->color;
+        $budget->bdt_current_expense = 0;
+        $budget->save();
+
+        return $budget;
     }
 }
