8d631e90769aef1d5076d8838865635ce7f70bf7
Matheus <matheusmartinscia@gmail.com>
2026-07-26 01:23:49 -0300

    Feat: Adiciona testes para criação de Budget

--- backend/tests/Unit/BudgetsServiceTest.php
+++ backend/tests/Unit/BudgetsServiceTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Unit;
 
+use App\Dto\Budget\CreateBudgetDTO;
+use App\Models\Budget;
 use App\Repositories\BudgetRepository;
 use App\Repositories\TransactionRepository;
 use App\Services\BudgetsService;
+use Error;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Mockery\MockInterface;
@@ -22,5 +25,57 @@ class BudgetsServiceTest extends MockeryTestCase {
         $this->service = new BudgetsService($this->budgetRepository, $this->transactionRepository);
     }
 
-    
+    public function test_create_budget_successfully(): void 
+    {
+        $log = Mockery::mock('alias:App\Logging\LogInvoker');
+        $log->shouldReceive('create->withPayload->withResponse->save');
+
+        $dto = CreateBudgetDTO::fromArray([
+            'id' => 1,
+            'name' => 'Transporte',
+            'limit' => 'R$ 3.450,00',
+            'color' => 'azul',
+        ]);
+
+        $budget = new Budget();
+        $budget->bdt_id = 1;
+        $budget->bdt_use_id = 1;
+        $budget->bdt_name = 'Transporte';
+        $budget->bdt_limit = 3450.00;
+        $budget->bdt_color = 'ESMERALDA';
+
+        $this->budgetRepository->shouldReceive('register')
+                                ->once()
+                                ->with($dto)
+                                ->andReturn($budget);
+
+        $response = $this->service->create($dto);
+
+        $this->assertTrue($response->getStatus());
+        $this->assertSame('Orçamento criado com sucesso', $response->getMessage());
+        $this->assertSame(201, $response->getCode());
+    }
+
+    public function test_create_budget_with_error(): void 
+    {
+        $log = Mockery::mock('alias:App\Logging\LogInvoker');
+        $log->shouldReceive('create->withPayload->save');
+
+        $dto = CreateBudgetDTO::fromArray([
+            'id' => 1,
+            'name' => 'Transporte',
+            'limit' => 'R$ 3.450,00',
+            'color' => 'azul',
+        ]);
+
+        $this->budgetRepository->shouldReceive('register')
+                                ->once()
+                                ->with($dto)
+                                ->andThrow(Error::class);
+
+        $response = $this->service->create($dto);
+
+        $this->assertFalse($response->getStatus());
+        $this->assertSame('Erro ao criar novo orçamento', $response->getMessage());
+    }
 }
