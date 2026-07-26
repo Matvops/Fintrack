d5d02b0daffdd9825bff4c0a3efb5f8d9755ea5c
Matheus <matheusmartinscia@gmail.com>
2026-07-26 01:21:46 -0300

    Feat: Cria DTO para o DTO

---
+++ backend/app/Dto/Budget/CreateBudgetDTO.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Dto\Budget;
+
+use App\Utils\Functions;
+
+readonly class CreateBudgetDTO {
+
+    public function __construct(
+        public int $id,
+        public string $name,
+        public string $limit,
+        public string $color,
+    )
+    {}
+
+    public static function fromArray(array $array): self 
+    {
+        return new self(
+            id: $array['id'],
+            name: $array['name'],
+            limit: Functions::formatValue($array['limit']),
+            color: strtoupper($array['color']),
+        );
+    }
+    
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'limit' => $this->limit,
+            'color' => $this->color,
+        ];
+    }
+}
