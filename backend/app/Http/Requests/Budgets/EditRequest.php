4ef49615467aa4f5c7eac76937dc4dbb058a0ed6
Matheus <matheusmartinscia@gmail.com>
2026-07-27 21:51:49 -0300

    Feat: Altera campos validados

--- backend/app/Http/Requests/Budgets/EditRequest.php
+++ backend/app/Http/Requests/Budgets/EditRequest.php
@@ -23,22 +23,22 @@ class EditRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'bdt_id' => 'required|exists:budgets,bdt_id',
-            'bdt_name' => 'required',
-            'bdt_color' => 'required|in:ambar,rosa,violeta,esmeralda,azul',
-            'bdt_limit' => 'required',
+            'id' => 'required|exists:budgets,bdt_id',
+            'name' => 'required',
+            'color' => 'required|in:ambar,rosa,violeta,esmeralda,azul',
+            'limit' => 'required',
         ];
     }
 
     public function messages(): array
     {
         return [
-            'bdt_id.required' => 'O id é obrigatório',
-            'ibdt_idd.exists' => 'Erro ao editar orçamento',
-            'bd_name.required' => 'O nome é obrigatório',
-            'bdt_limit.required' => 'O limite é obrigatório',
-            'bdt_color.required' => 'A cor é obrigatória',
-            'bdt_color.in' => 'A cor é inválida'
+            'id.required' => 'O id é obrigatório',
+            'id.exists' => 'Erro ao editar orçamento',
+            'name.required' => 'O nome é obrigatório',
+            'limit.required' => 'O limite é obrigatório',
+            'color.required' => 'A cor é obrigatória',
+            'color.in' => 'A cor é inválida'
         ];
     }
 }
